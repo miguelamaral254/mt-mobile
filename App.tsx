17ace63233a3a9c70790b658bd8999a20a3804f1
Miguel <miguel.amaral.sales@gmail.com>
2024-10-16 23:36:49 -0300

    configuração inicial
    aqui temos o Expo com Typescript e Nativewind(tailwind)

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 justify-center items-center bg-gray-300'>
+      <Text className='text-red-700'>SENAC POAAAAAAAAAAAAA!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
